4e432b10edb0b25b7f8f811c08f147665619b168
m2357aaa <m2357kinu8810@yahoo.co.jp>
2025-12-19 15:59:04 +0900

    works下層css function.php追記

--- page-service-sdgs.php
+++ page-service-sdgs.php
@@ -4,42 +4,41 @@
         <section id="serive" class="first_section">
             <div class="page_title_wrap">
                 <div class="page_title">
-                    <h2 class="page_title_main">CONTENT</h2>
-                    <p class="page_title_sub">コンテンツ制作事業</p>
+                    <h2 class="page_title_main">SDGs</h2>
+                    <p class="page_title_sub">SDGs推進事業</p>
                 </div>
             </div>
             <div class="service_description">
-                <p>自社にストックされているスポットやトレンド情報などを、コンテンツとしてさまざまなメディアへ展開。</p>
+                <p>日本からハワイのSDGsの普及、啓発、達成にチャレンジする取り組みを実施。</p>
             </div>
             <div class="lower_kv">
                 <picture>
-                    <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/service-contents/kv-sp.webp" media="(max-width: 767px)">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-contents/kv.webp" alt="コンテンツ制作事業">
+                    <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/service-sdgs/kv-sp.webp" media="(max-width: 767px)">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-sdgs/kv.webp" alt="コンテンツ制作事業">
                 </picture>
             </div>
             <div class="service_content">
-                <div class="service_inner">
+                <div class="service_inner service_inner1">
                     <div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-contents/contents1.webp" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-sdgs/sdgs1.webp" alt="">
                     </div>
                     <div>
-                        <h3>ストック情報の<br>他メディア展開</h3>
+                        <h3>事業を通じて ハワイ州<br>「Aloha+Challenge」に貢献</h3>
                         <p>
-                            国内外の最新スポットやトレンド、「ハワイ」「東京」「カフェ」「ラーメン」「スイーツ」「温泉」などの人気キーワードの情報が社内に収集されています。<br>
-                            そうした情報を紙メディア、デジタルコンテンツの特集記事や地図情報アプリなどスマホ用コンテンツとして提供させていただくことができます。
+                            SDGs先進州であるハワイ州では、<br>「Aloha+Challenge」という、持続可能な目標を達成するための特別プログラムを設定しています。長年ハワイとともに歩んできた当社は、自社メディアやコンテンツを通しての啓蒙活動を軸に、日本からハワイへの貢献、目標の達成に事業活動を通じてチャレンジしています。
                         </p>
                     </div>
                 </div>
-                <div class="service_inner">
+                <div class="service_inner service_inner2">
                     <div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-contents/contents2.webp" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/service-sdgs/sdgs2.webp" alt="">
                     </div>
                     <div>
-                        <h3>最大限に価値を伝える<br>映像コンテンツ</h3>
+                        <h3>廃棄物ゼロを実現する<br>再資源化装置の導入支援</h3>
                         <p>
-                            ご要望やご予算に合わせて、企画からキャスティング等すべて対応いたします。企画・シナリオ～撮影、編集作業すべてお任せください。<br>
-                            クオリティ重視のブランディング動画や、サービス紹介動画など実写・アニメーション形式問わず制作が可能です。まずはご相談ください。
+                            地域のさまざまなごみ問題を解決する再資源化させる装置の導入を支援・推進しています。あらゆる廃棄物を分別することなく、本装置で処理し、肥料などの有価資源を生産します。その有価資源は、新たにその地域のエネルギーとなり循環。サステナブルな地域社会の構築に貢献いたします。
                         </p>
+                        <a class="service_btn" href="">詳細はこちら</a>
                     </div>
                 </div>
             </div>
